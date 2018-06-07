46e6a41aad8879571649710fa94356fcc2083dd3
Martin Wilke <miwi@ixsystems.com>
2018-06-08 04:44:32 +0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,8 +4,9 @@
 sysrc -f /etc/rc.conf clamav_freshclam_enable="YES"
 sysrc -f /etc/rc.conf clamav_clamd_enable="YES"
 
+#update database before start
+freshclam 
+
 # Start the service
 service clamav-freshclam start 2>/dev/null
-# Sleep 15 sec to allow freshclam to sync the database
-sleep 15
 service clamav-clamd start 2>/dev/null
